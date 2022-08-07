cd381ff8408c6b0ef0515d4008e6a00275658b99
Andrew Mahabee <mahabeean@live.com>
2022-08-06 21:54:38 -0400

    -changed output formatting -truncated output values to represent $xx.xx

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import gross
 import fed
 
 print(" ")
-print("-------------------------")
-print(" ")
 print("Your basepay for this pay period will be: $", gross.basepay.reg_basepay)
+print(" ")
 print("You work", int(gross.basepay.ot_hours), "hours of overtime this pay period!")
 print("Your OT pay for this pay period will be: $", gross.basepay.ot_basepay)
+print(" ")
 print("Your weekend pay differential will be: $", gross.differential.weekend_pay)
 print("Your evening pay differential will be: $", gross.differential.evening_pay)
 print("Your overnight pay differential will be: $", gross.differential.overnight_pay)
 print(" ")
 print("Your total gross pay for this pay period will be: $", gross.gross_pay)
-print("Your total federal tax deduction = $", gross.gross_pay - (fed.fedpay.net_pay / gross.basepay.pay_periods))
-print("Your total net pay for this pay period is: $", fed.fedpay.net_pay / gross.basepay.pay_periods)
+
+fed_tax_deduction = gross.gross_pay - (fed.fedpay.net_pay / gross.basepay.pay_periods)
+total_net_pay = float(fed.fedpay.net_pay / gross.basepay.pay_periods)
+
+print("Your total federal tax deduction = $", float(f'{fed_tax_deduction:.2f}'))
+print("Your total net pay for this pay period is: $", float(f'{total_net_pay:.2f}'))
