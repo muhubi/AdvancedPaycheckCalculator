b783d9ae6b4cf909259961990e78b2e84368deef
Andrew Mahabee <mahabeean@live.com>
2022-08-13 16:34:29 -0400

    added a print state for state tax type and added state file to main file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gross
 import fed
+import state
 
 print(" ")
 print("-----------------------------------------------------------------------")
@@ -13,10 +14,11 @@ print("Your evening pay differential will be: $", gross.differential.evening_pay
 print("Your overnight pay differential will be: $", gross.differential.overnight_pay)
 print(" ")
 print("Your total gross pay for this pay period will be: $", gross.gross_pay)
-
+print(" ")
 fed_tax_deduction = gross.gross_pay - (fed.fedpay.net_pay / gross.basepay.pay_periods)
 total_net_pay = float(fed.fedpay.net_pay / gross.basepay.pay_periods)
-
 print("Your total federal tax deduction = $", float(f'{fed_tax_deduction:.2f}'))
+print(state.filers_state.state_tax_type)
+print(" ")
 print("Your total net pay for this pay period is: $", float(f'{total_net_pay:.2f}'))
 print("-----------------------------------------------------------------------")
